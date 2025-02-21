8d93325d6fe12db4d8a615ad0201bb51292a90a2
Ezequiel Orellana <124017711+EZR4N@users.noreply.github.com>
2025-02-21 19:47:17 -0300

    Actualizar CartItem.jsx

--- src/componentes/CartItem/CartItem.jsx
+++ src/componentes/CartItem/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useContext } from 'react'
-import { CarritoContext } from '../../context/carritoContext'
+import { CarritoContext } from '../../context/CarritoContext'
 import '../CartItem/CartItem.scss'
 
 const CartItem = ({item, cantidad}) => {
