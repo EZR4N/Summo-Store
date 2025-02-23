bf73d96549f2cc222b16dfc438f8ae3b9e3aba8b
EZR4N <ezeore2@gmail.com>
2025-02-23 13:42:16 -0300

    correcion de imagenes

--- src/componentes/main/MainContainerBanner/MainContainerBanner.jsx
+++ src/componentes/main/MainContainerBanner/MainContainerBanner.jsx
@@ -4,7 +4,6 @@ const MainContainerBanner = () => {
   return (
     <div className='mainContainerBanner'>
         <img src="\assets\imagenes\Frame 41.png" alt="banner Summo ayuda" />
-        <img src="\assets\imagenes\SummoApp.png" alt="banner Summo app" />
     </div>
   )
 }
